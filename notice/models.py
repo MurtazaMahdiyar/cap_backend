8d75bf06bcd11389df9646e4bc013ef551f2d206
Murtaza Mahdiyar <murtaza.mahdiyar@gmail.com>
2023-10-22 06:36:19 +0430

    bugs fixed 22-10-2023

--- notice/models.py
+++ notice/models.py
@@ -14,7 +14,7 @@ class Notice(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     attachment = models.FileField(upload_to='notice/%Y/%m/%d', blank=True)
-    registry_date = models.DateField(auto_now_add=True)
+    registry_date = models.DateTimeField(auto_now_add=True)
 
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     audience = models.CharField(max_length=20, choices=AudienceChoices.choices)
